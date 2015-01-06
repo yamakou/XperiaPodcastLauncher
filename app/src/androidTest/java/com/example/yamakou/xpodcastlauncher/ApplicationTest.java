8e0e66afe98f652908bf3646fe9e919001e90ec8
yamakou <yamakou@example.com>
2015-01-06 10:39:24 +0900

    Rename to xpodcastlauncher.

--- app/src/androidTest/java/com/example/yamakou/xpodcastlauncher/ApplicationTest.java
+++ app/src/androidTest/java/com/example/yamakou/xpodcastlauncher/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.example.yamakou.xperiapodcastlauncher;
+package com.example.yamakou.xpodcastlauncher;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/main/java/com/example/yamakou/xpodcastlauncher/MainActivity.java
+++ app/src/main/java/com/example/yamakou/xpodcastlauncher/MainActivity.java
@@ -1,10 +1,8 @@
-package com.example.yamakou.xperiapodcastlauncher;
+package com.example.yamakou.xpodcastlauncher;
 
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
