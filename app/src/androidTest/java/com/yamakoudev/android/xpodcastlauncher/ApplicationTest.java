990a52884553b1d4782da229c4010db58d8adcd0
yamakou <yamakou@example.com>
2015-01-07 15:26:47 +0900

    Rename package name.

--- app/src/androidTest/java/com/yamakoudev/android/xpodcastlauncher/ApplicationTest.java
+++ app/src/androidTest/java/com/yamakoudev/android/xpodcastlauncher/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.example.yamakou.xpodcastlauncher;
+package com.yamakoudev.android.xpodcastlauncher;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/main/java/com/yamakoudev/android/xpodcastlauncher/MainActivity.java
+++ app/src/main/java/com/yamakoudev/android/xpodcastlauncher/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.yamakou.xpodcastlauncher;
+package com.yamakoudev.android.xpodcastlauncher;
 
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
